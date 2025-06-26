c41d5e0b04493b54ec12fdf3c11a364668eff4c8
Eze <a.ezequielirace@gmail.com>
2025-06-26 18:59:35 -0300

    update component test

--- components/Test.server.vue
+++ components/Test.server.vue
@@ -1,3 +1,11 @@
+<template>
+<div class="p-4">
+  <h1 class="text-2xl font-bold mb-4">Test Component</h1>
+  <p>This is a test component to verify server-side rendering.</p>
+   <p>Current time: {{ new Date().toLocaleTimeString() }}</p>
+   </div>   
+</template>
+
 <script setup>
 
 </script>
