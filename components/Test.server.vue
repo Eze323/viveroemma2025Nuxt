13b300aa3fc5585a0dac462c8890276c3308725c
Eze <a.ezequielirace@gmail.com>
2025-06-26 18:58:03 -0300

    update api

--- components/Test.server.vue
+++ components/Test.server.vue
@@ -1,3 +1,13 @@
+<template>
+  <div>
+    <h1>Test Component</h1>
+    <p>This is a server-side rendered component.</p>
+  </div>
+</template>
+
 <script setup>
+//funcion que llama a api
+
+
 
 </script>
