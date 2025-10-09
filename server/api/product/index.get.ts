8a781c6a9c4857329f804f94a3178e0762f30c65
ALBERTO EZEQUIEL IRACE <103865149+Eze323@users.noreply.github.com>
2025-10-08 23:04:15 -0300

    update index product

--- server/api/product/index.get.ts
+++ server/api/product/index.get.ts
@@ -5,51 +5,58 @@ import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
 
-if (!process.env.JWT_SECRET) {
-  throw new Error('JWT_SECRET no está definido en las variables de entorno');
-}
+
+
+
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export default defineEventHandler(async (event: H3Event) => {
   try {
+    
+    if (!JWT_SECRET) {
+  throw new Error('JWT_SECRET no está definido en las variables de entorno');
+}
+    
     // Validar token JWT
-    const authHeader = event.node.req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      throw createError({
-        statusCode: 401,
-        statusMessage: 'Token de autenticación faltante o inválido',
-      });
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, JWT_SECRET);
+
+
+    // const authHeader = event.node.req.headers.authorization;
+    // if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    //   throw createError({
+    //     statusCode: 401,
+    //     statusMessage: 'Token de autenticación faltante o inválido',
+    //   });
+    // }
+    // const token = authHeader.split(' ')[1];
+    // jwt.verify(token, JWT_SECRET);
 
     // Obtener productos publicados
     const productRecords = await prisma.products.findMany({
-      // where: {
-      //   publicado: true,
-      // },
-      // select: {
-      //   id: true,
-      //   name: true,
-      //   precio_venta: true,
-      //   stock: true,
-      // },
+      where: {
+        publicado: true,
+      },
+      select: {
+        id: true,
+        name: true,
+        precio_venta: true,
+        stock: true,
+      },
     });
 
     // Mapear los datos al formato esperado por el frontend
-    // const products = productRecords.map(product => ({
-    //   id: product.id,
-    //   name: product.name,
-    //   unit_price: Number(product.precio_venta), // Convertir Decimal a número
-    //   stock: product.stock,
-    // }));
+    const products = productRecords.map(product => ({
+      id: product.id,
+      name: product.name,
+      unit_price: Number(product.precio_venta), // Convertir Decimal a número
+      stock: product.stock,
+    }));
 
     return {
       success: true,
       data: productRecords,
     };
   } catch (error) {
-    console.error('Error en GET /api/products:', error);
+    console.error('Error en GET /api/product:', error);
     const err = error as { statusCode?: number; statusMessage?: string };
     throw createError({
       statusCode: err.statusCode || 500,
